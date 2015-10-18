8eb01adc08c9e70ba6848374f7d96395d3f6c5f3
Jan Marten <jan.marten@gmx.de>
2015-10-18 19:17:07 +0200

    Make videomidinotemapping file serializable

--- src/main/java/io/github/leovr/vlcmidi/VideoMidiNoteMapping.java
+++ src/main/java/io/github/leovr/vlcmidi/VideoMidiNoteMapping.java
@@ -1,17 +1,26 @@
 package io.github.leovr.vlcmidi;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.github.leovr.vlcmidi.midi.MidiNote;
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.File;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class VideoMidiNoteMapping {
 
-    private File file;
+    public VideoMidiNoteMapping(final File file, final MidiNote midiNote) {
+        this.filePath = file.getAbsolutePath();
+        this.midiNote = midiNote;
+    }
+
+    private String filePath;
     private MidiNote midiNote;
+
+    @JsonIgnore
+    public File getFile() {
+        return new File(filePath);
+    }
 }
