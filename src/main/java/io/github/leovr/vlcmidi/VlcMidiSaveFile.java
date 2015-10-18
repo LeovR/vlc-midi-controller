047be036862a7a8340f012d1c1522e53c6e9cbee
Jan Marten <jan.marten@gmx.de>
2015-10-18 19:16:54 +0200

    Add save file model class

---
+++ src/main/java/io/github/leovr/vlcmidi/VlcMidiSaveFile.java
@@ -0,0 +1,16 @@
+package io.github.leovr.vlcmidi;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class VlcMidiSaveFile {
+
+    private List<VideoMidiNoteMapping> mappings = new ArrayList<>();
+}
